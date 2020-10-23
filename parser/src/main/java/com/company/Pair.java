dfb4de24faf33a002b419669f24d49fec64159b3
ilya <zaporozhecilya@gmail.com>
2020-10-23 22:02:14 +0300

    fixed pair

--- parser/src/main/java/com/company/Pair.java
+++ parser/src/main/java/com/company/Pair.java
@@ -1,25 +1,31 @@
-class Pair {
+package com.company.parser;
+
+public class Pair {
 	private String link;
 	private String header;
 
 	public Pair(String link, String header) {
-		this->link = link;
-		this->header = header;
+		this.link = link;
+		this.header = header;
 	}
 
 	public String getLink() {
-		return this->link;
+		return this.link;
 	}
 
 	public String getHeader() {
-		return this->header;
+		return this.header;
 	}
 
 	public void setLink(String link) {
-		this->link = link;
+		this.link = link;
 	}
 
 	public void setHeader(String header) {
-		this->header = header;
+		this.header = header;
+	}
+
+	public String toString() {
+		return this.link + " " + this.header;
 	}
 }
