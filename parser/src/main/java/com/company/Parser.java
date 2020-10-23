9dfaeca8fd038789270b1cd40be95972ec8cdd6c
ilya <zaporozhecilya@gmail.com>
2020-10-23 19:17:03 +0300

    mod. parseToFile

--- parser/src/main/java/com/company/Parser.java
+++ parser/src/main/java/com/company/Parser.java
@@ -4,57 +4,46 @@ import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 import org.jsoup.Jsoup;  
 import org.jsoup.nodes.Document;
+import java.util.ArrayList;
+import java.io.IOException;
 
 public class Parser {
 	private String url;
 	private String dir;
-	ArryList<String> parsedLinks;
+	ArrayList<String> parsedLinks;
 
 	public Parser() {
-		this->url = "";
-		this->dir = "";
-		this->parsedLinks = new ArryList<>();
+		this.url = "";
+		this.dir = "";
+		this.parsedLinks = new ArrayList<>();
 	}
 
-	public setAdress(String url) {
-		this->url = url;
+	public void setAdress(String url) {
+		this.url = url;
 	}
 
-	public setPath(String path) {
-		this->dir = path;
+	public void setPath(String path) {
+		this.dir = path;
 	}
 
-	public parseToFiles() {
-		Document doc = Jsoup.connect(this->url).get();  
+	public void parseToFiles() throws IOException {
+		Document doc = Jsoup.connect(this.url).get();  
 		Element postEntry = doc.getElementsByClass("post-entry").first();
-		Element linkBlocks = postEntry.child(1).children();
-		Strink link;
+		Elements linkBlocks = postEntry.getElementsByTag("a");
+		String link;
        	for (Element iter : linkBlocks) {
-       		link = iter.html();
+       		link = iter.attr("href");
        		if (!parsedLinks.contains(link)) {
-       			//parsePage(link, )
-       			System.out.println(link);
+       			parsedLinks.add(link);
        		}
+       	}
+       	String str;
+       	for (int i = 0; i < parsedLinks.length() - 1; i++) {
+       		str = parsePage(parsedLinks[i]);
+       		//System.out.println(str);
        	}
 	}
 
-	private parsePage(Document doc, String path) {
-		Element postEntry = doc.getElementsByClass("post-entry").first();
-
-        Element dataBlock = postEntry.child(1);
-
-        Block block = new Block()
-        StringBuilder str = new StringBuilder(this->parseBlock(dataBlock));
-
-        try(PrintWriter writer = new PrintWriter(path, "UTF-8")) {
-            writer.println(str);
-        } 
-        catch(IOException ex){   
-            System.out.println(ex.getMessage());
-        }   
-	}
-
-	private parseBlock(Element datdBlock) {
-		
+	private void parsePage(Document doc) {
 	}
 }
